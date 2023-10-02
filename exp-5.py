c7636e0eb0b17d421d7d5ff1b75693ae020fa18d
Ayush Sharma <146195302+ninjayush@users.noreply.github.com>
2023-10-02 17:56:25 +0530

    Update exp-5.py

--- exp-5.py
+++ exp-5.py
@@ -1,1 +1,8 @@
+import mysql.connector
+con=mysql.connector.connect(host='localhost', user='root', password='1234')
+cur=con.cursor()
+cur.execute('use superchaina')
+cur.execute('select * from student;')
+x=cur.fetchall()
+for i in x: print(i)
 
