a86e937c137e965afe1c3c55703442ed7345a235
Ayush Sharma <146195302+ninjayush@users.noreply.github.com>
2023-10-02 17:57:20 +0530

    Update exp-5.py

--- exp-5.py
+++ exp-5.py
@@ -1,8 +1,8 @@
-import mysql.connector
-con=mysql.connector.connect(host='localhost', user='root', password='1234')
+import mysql.connector as ps
+con=ps.connect(host='localhost', user='root', password='1234')
 cur=con.cursor()
 cur.execute('use superchaina')
-cur.execute('select * from student;')
-x=cur.fetchall()
-for i in x: print(i)
+cur.execute('delete from student where stname="Abc";')
+con.commit()
+print("student named Abc deleted")
 
